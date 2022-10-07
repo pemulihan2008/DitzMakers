ccaeb3b2ff4ec7e9e4f247c15a9f0ea76bbeb376
pemulihan2008 <110373717+pemulihan2008@users.noreply.github.com>
2022-10-08 01:07:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "𝘿𝙞𝙩𝙯" //namabot kalian
+global.ownername= "𝘿𝙞𝙩𝙯 𝙈𝙖𝙠𝙚𝙧𝙨" //nama kalian
+global.myweb ="https://www.tiktok.com/@adit.offlclal" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCRJNLP1ko2rx0j9YxKEJ8Ew" //bebas asal jan hapus
+global.myweb2 = "https://www.instagram.com/aditya.farabi_2008" //bebas
+global.email = "adit.official2008@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6287748821221'] //premium user
+global.owner = ["6287748821221","6281278265691"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6287748821221" // nomor wa kalian
+global.ownernomerr = "+6287748821221" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© 𝘿𝙞𝙩𝙯' //sticker wm ubah
+global.author = '𝘿𝙞 𝘽𝙪𝙖𝙩 𝙊𝙡𝙚𝙝 𝘿𝙞𝙩𝙯 𝙈𝙖𝙠𝙚𝙧𝙨' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
